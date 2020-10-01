2d018473c38751b1aefed3c264d3f03f67bbcc59
FrancaTM <francatm@outlook.com>
2020-10-01 18:58:21 -0300

    add simple animation to button

--- Xylophone iOS/ViewController.swift
+++ Xylophone iOS/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            sender.alpha = 1.0
+        }
     }
 
     func playSound(soundName: String) {
